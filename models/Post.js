e409fac91ca7f6958ec4c288c15c3dfc806da088
terbinos <kingterbinos06@gmail.com>
2020-03-25 12:17:32 +0300

    Added files

---
+++ models/Post.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create schema
+const PostSchema = new Schema({
+    user:{
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    text: {
+        type: String,
+        required: true,
+    },
+    name:{
+        type: String,
+
+    },
+    avatar:{
+        type: String
+    },
+    comments:[
+        {
+            user:{
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name:{
+                type: String,
+ 
+            },
+            avatar:{
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+
+    ],
+    likes:[
+        {
+            user:{
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            }
+        }
+
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Post = mongoose.model('post',PostSchema);
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const app = express();
+
+// BOdy parser middleware
+app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.json());
+// DB Config
+const db = require('./config/keys');
+
+// Connect to MongoDB
+mongoose
+    .connect(db.mongoURI)
+    .then(() => console.log('Successful Database Connection '))
+    .catch(err => console.log(err));
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require('./config/passport')(passport);
+
+app.get('/',(req,res) => res.send('Hello there'));
+
+// Use Routes
+app.use('/api/users',users);
+app.use('/api/profile',profile);
+app.use('/api/posts',posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port,() => console.log(`Server running on port ${port}`));
